8d088d168bd29d1cbfd502fd0e12f3b3521916b1
Samuelson <samuelsonma@gmail.com>
2017-03-09 23:27:16 -0400

    trabalhando com date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,11 +1,32 @@
 class NegociacaoController{
 
+    constructor(){
+
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
 
 
     adiciona(event){
 
         event.preventDefault();
-        alert('chamei ação no controller');
+
+        let data = new Date(...this._inputData.value.split('-')
+        .map((item, indice)=> {
+            return item - indice * 2;
+        }));
+
+        console.log(data);
+
+        
+
+        //negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+
+        //console.log(negociacao);
 
     }
 }
