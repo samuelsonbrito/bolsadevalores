e8ecf85f410c064836bb115a1814a5b4b9d51c3f
Samuelson Brito <samuelsonma@gmail.com>
2017-03-25 17:46:30 -0400

    melhorando view

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,5 +1,11 @@
 class MensagemView extends View{
 
+    constructor(elemento){
+
+       super(elemento);
+
+    }
+
     _template(model){
         return model.texto ? `<p class="elert alert-info">${model.texto}</p>` : '<p></p>';
     }
--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -5,6 +5,10 @@ class View {
 
     }
 
+    _template(){
+        throw new Error('O método _template() dever ser implementado.');
+    }
+
 
     update(model){
         this._elemento.innerHTML = this._template(model);
